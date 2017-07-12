6a291e2f2d2590f0aa38c9767fee5cc2126275bf
Gurpreet Singh <gphailer@outlook.com>
2017-07-11 23:40:40 -0500

    My Github Hello World App

---
+++ MyGitHubHelloWorld/MyGitHubHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyGitHubHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
